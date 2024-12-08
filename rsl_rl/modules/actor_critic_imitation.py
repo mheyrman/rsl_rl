bc537d76b6001689cdc3c2873727fa4722ead388
Matthias Heyrman <mcaro.heyrman@gmail.com>
2024-12-08 09:11:14 +0100

    wavelets and gaussian encoders in parallel

--- rsl_rl/modules/actor_critic_imitation.py
+++ rsl_rl/modules/actor_critic_imitation.py
@@ -41,7 +41,7 @@ class ImitationAgent(nn.Module):
         dwt_size = test_out_l.squeeze(1).reshape(4096, -1).shape[1]
         for i in range(len(test_out_h)):
             dwt_size += test_out_h[i].squeeze(1).reshape(4096, -1).shape[1]
-        mlp_input_dim = num_state_obs + dwt_size
+        mlp_input_dim = num_state_obs + dwt_size + encoder_hidden_dims[-1]
 
         # mlp_input_dim = num_state_obs + encoder_hidden_dims[-1]
 
@@ -54,7 +54,7 @@ class ImitationAgent(nn.Module):
         # )
         # self.pae_enc = utils.PAE(self.num_reference_obs, encoder_hidden_dims[-1])
         self.phase_enc = utils.PeriodicEncoder(self.num_reference_obs, latent_channels=self.latent_channels, horizon=self.horizon)
-        # self.obs_enc = utils.GaussianEncoderBlock(self.num_reference_obs, encoder_hidden_dims[-1], encoder_dims=encoder_hidden_dims)
+        self.obs_enc = utils.GaussianEncoderBlock(self.num_reference_obs, encoder_hidden_dims[-1], encoder_dims=encoder_hidden_dims)
         # self.obs_enc = utils.GRUBlock(self.num_reference_obs, encoder_hidden_dims[2], 128)
         # self.obs_enc = utils.TransformerEncoder(self.num_reference_obs, encoder_hidden_dims[2])
         # self.obs_enc = utils.PAE(self.num_reference_obs, encoder_hidden_dims[-1])
@@ -91,10 +91,10 @@ class ImitationAgent(nn.Module):
         # split reference obs and obs, pass reference obs through encoder, concatenate with obs and pass through policy
         ref_obs, obs = torch.split(x, [self.num_reference_obs, x.size(1) - self.num_reference_obs], dim=1)
         periodic_out = self.phase_enc(ref_obs)
-        # enc_out = self.obs_enc(ref_obs)
+        enc_out = self.obs_enc(ref_obs)
         # sum the two encodings
-        # ref_obs = torch.cat([periodic_out, enc_out], dim=1)
-        x = torch.cat([periodic_out, obs], dim=1)
+        ref_obs = torch.cat([periodic_out, enc_out], dim=1)
+        x = torch.cat([ref_obs, obs], dim=1)
 
         # x = self.input_embedding(x)
         # x = x + self.pos_encoding
